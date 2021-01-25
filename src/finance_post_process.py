e6486e1e4cba522f3f84788507634b6923d2645a
Aditya Warnulkar <adwarnulkar@gmail.com>
2021-01-24 17:25:50 -0800

    Updates finance modeling

--- src/finance_post_process.py
+++ src/finance_post_process.py
@@ -1,5 +1,12 @@
 import numpy as np
 import pandas as pd
+import itertools
+from bokeh.io import output_notebook , show
+from bokeh.models import ColumnDataSource, FactorRange
+from bokeh.plotting import figure
+from bokeh.transform import factor_cmap
+from bokeh.models import HoverTool
+from bokeh.palettes import Spectral6
 
 def get_finance_df (start_year=2019,years_of_service=20,joining_salary=105000,tax_rate=30,
                    monthly_expense=3000,expense_rate=2,growth_rate=8,cash_retain_perc=60):
@@ -36,3 +43,23 @@ def get_finance_df (start_year=2019,years_of_service=20,joining_salary=105000,ta
     df['ROI'] = df['cash_invested'] * ((1 + growth_rate / 100)**years_of_service)
     
     return df.astype(int)
+
+def plot_expense(df_expense):
+    category = list(df_expense.index)
+    expenses = ['budget','amount']
+    data = {'category' : category,'budget' : df_expense.budget , 'amount' : df_expense.Amount}
+    
+    x = [ (cat, expense) for cat in category for expense in expenses ]
+    counts = list(itertools.chain(*zip(data['budget'], data['amount'])))
+   
+    source = ColumnDataSource(data=dict(x=x, counts=counts))
+    
+    p = figure(x_range=FactorRange(*x), plot_height=400, plot_width=950,title="Expense Report")
+    p.vbar(x='x', top='counts', width=0.9, source=source, color='red' ,line_color="white",
+          fill_color=factor_cmap('x', palette=Spectral6, factors=expenses, start=1, end=2))
+    p.add_tools(HoverTool(tooltips=[("CATEGORY", "@x"), ("VALUE", "@counts")]))
+    p.x_range.range_padding = 0.1
+    p.xaxis.major_label_orientation = 1
+    p.xgrid.grid_line_color = None
+    p.yaxis.axis_label = 'Expense ($)'
+    return(p)
